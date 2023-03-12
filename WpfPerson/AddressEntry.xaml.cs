d4fc2b5e10fb47e1ff328fcdeaa5216ffccc08da
Héctor Manuel Sandoval Landázuri <ingsan@live.com.mx>
2023-03-12 16:07:46 -0600

    Agregar archivos de proyecto.

---
+++ WpfPerson/AddressEntry.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfPerson
+{
+    /// <summary>
+    /// Lógica de interacción para AddressEntry.xaml
+    /// </summary>
+    public partial class AddressEntry : Window
+    {
+        ISaveAddress _parent;
+        public AddressEntry(ISaveAddress parent)
+        {
+            InitializeComponent();
+            _parent = parent;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            AddressModel address = new AddressModel
+            {
+                StreetAddress = txtStreetAddress.Text,
+                City = txtCity.Text,
+                State = txtState.Text,
+                PostalCode=txtCode.Text
+            };
+            _parent.SaveAddress(address);
+            this.Close();
+        }
+    }
+}
---
+++ WpfPerson/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPerson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,ISaveAddress
+    {
+        BindingList<AddressModel> addresses=new BindingList<AddressModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbxAddresses.ItemsSource = addresses;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddressEntry entry = new AddressEntry(this);
+            entry.Show();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                IsActive = (chkActive.IsChecked ?? false),
+                Addresses = addresses.ToList()
+            };   
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+            addresses.Add(address);
+        }
+    }
+}
